108b44b29636e34e9b73d926fbb33eb6e5be781f
hopwhitlock64 <whi15064@byui.edu>
2020-12-08 11:28:01 -0700

    alarm functions, good sched format, sched removes

--- FinalApp/app.js
+++ FinalApp/app.js
@@ -1,37 +1,38 @@
 // alarm sound
 var alarmSound = new Audio();
 alarmSound.src = 'mandalorian_theme.mp3';
-console.log(document.getElementById('sound').value);
+
 function changeSound(){
   var select = document.getElementById('sound').value;
-  document.getElementById('sound').innerHTML = select;
-  alarmSound.play();
-
+  alarmSound.src = select;
 }
 
 //main functions
 function setAlarm(button){
+  
   var date = document.getElementById('alarmDate').value;
 var time = document.getElementById('alarmTime').value;
 console.log(time);
 
 var alarm = new Date(date+'T'+time);
 var alarmTime = new Date(alarm.getFullYear(), alarm.getMonth(), alarm.getDate(), alarm.getHours(), alarm.getMinutes(), alarm.getSeconds(), alarm.getMilliseconds());
+var details = {title:document.getElementById("title").value, notes:document.getElementById("notes").value, alarmTime:alarmTime};
 var differenceinTime = alarmTime.getTime() - Date.now();
 console.log('current date'+ Date.now());
 console.log('alarm time'+ alarmTime.getTime());
 console.log(differenceinTime);
 if(alarmTime.getTime() < 0){
-  alert('Time had already passed!');
+  alert('Time has already passed!');
   return;
 }
 var times = [];
-if(times = JSON.parse(localStorage.getItem('arrayTimes'))){
+if(localStorage.getItem('arrayTimes')!= null && localStorage.getItem('arrayTimes').length > 0){
+  times = JSON.parse(localStorage.getItem('arrayTimes'));
   console.log(times);
 } else{
   times = [];
 }
-times.push(alarmTime);
+times.push(details);
 localStorage.setItem('arrayTimes', JSON.stringify(times));
 
 
--- FinalApp/schedules.js
+++ FinalApp/schedules.js
@@ -1,18 +1,30 @@
 var arrayTimes = JSON.parse(localStorage.getItem('arrayTimes'));
 console.log(localStorage.getItem('arrayTimes')); 
- 
+ var counter = 0;
 for(let item of arrayTimes){
     var listItem = document.createElement('LI');
     var cancelButton = document.createElement('BUTTON');
-    cancelButton.innerHTML = 'cancel';
-    listItem.innerHTML = item;
+    cancelButton.innerHTML = 'remove';
+    var date = new Date(item.alarmTime);
+    listItem.innerHTML = "Title:    " + item.title + "<br>" + "Date:    " + date.getMonth() + " - " + date.getDate() + " - "  + date.getFullYear() +
+     "<br> " + "Time:   " + date.getHours() + " : " + date.getMinutes() + " : " + date.getSeconds() 
+     + "<br> " + "Notes:  " + item.notes;
     listItem.appendChild(cancelButton);
+    cancelButton.setAttribute("class", "removeBtn");
+    cancelButton.setAttribute("id", counter + "removeAlarm");
+    listItem.setAttribute("id", counter);
     document.getElementById('schedules').appendChild(listItem);
+    document.getElementById(counter).addEventListener("click", cancelAlarm)
+    counter++;
 }
 
-function cancelAlarm(button){
-    button.innerHTML = 'Set Alarm';
-    button.setAttribute('onclick', 'setAlarm(this);');
+function cancelAlarm(event){
+    // button.innerHTML = 'Set Alarm';
+    // button.setAttribute('onclick', 'setAlarm(this);');
+    var item = event.target.id.slice(0,-11);
+    document.getElementById(item).remove();
+    arrayTimes.splice(parseInt(item, 10), 1);
+    localStorage.setItem('arrayTimes', arrayTimes);
   }
 
 //   button.innerHTML = 'Cancel Alarm';
